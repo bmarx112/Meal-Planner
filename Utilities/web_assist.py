610ed789e4a61e6a6bb49185e3631905e9543cbc
Brad Marx_II <bradmarx112@gmail.com>
2021-07-26 12:33:55 -0700

    added new property for RecipeWebScrapeManager. Added new function for web_assist.

--- Utilities/web_assist.py
+++ Utilities/web_assist.py
@@ -3,13 +3,16 @@ import ssl
 from urllib.request import urlopen
 import re
 import warnings
+import csv
+from typing import Union
 
 
 def make_context() -> ssl.SSLContext:
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
-        return ctx
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    return ctx
+
 
 def get_html_for_soup(url: str, ct, suffix: str = ''):
     formatted_url = url + suffix
@@ -17,9 +20,12 @@ def get_html_for_soup(url: str, ct, suffix: str = ''):
     soup = BeautifulSoup(html, "html.parser")
     return soup
 
-def find_in_url(url: str, item: int = -1, cleanup: bool = True) -> str:
-    elements = re.findall(r"[^/]+", url)
 
+def find_in_url(url: str,
+                item: int = -1,
+                cleanup: bool = True) -> str:
+
+    elements = re.findall(r"[^/]+", url)
     try:
         target = elements[item]
     except:
@@ -28,18 +34,31 @@ def find_in_url(url: str, item: int = -1, cleanup: bool = True) -> str:
         target = elements[-1]
     
     if cleanup:
-        target = re.sub(r'[^A-Za-z0-9]{1}', " ", target)
+        target = re.sub(r'[^A-Za-z0-9]', " ", target)
 
     return target
 
-def amend_url(base_url: str, prefix: str) -> str:
+
+def prepend_root_to_url(base_url: str, prefix: str) -> str:
 
     if base_url[0] == '/':
         url = prefix + base_url
     else:
         url = base_url
-    
     return url
+
+# TODO: Make this function able to write dictionaries of any form/nesting to csv
+
+
+def write_to_csv(filename: str, data: dict, headers: Union[None, list]):
+
+    with open(filename, 'w') as f:
+        if headers is not None:
+            column_names = ','.join(headers) + '\n'
+            f.write(column_names)
+        for i, r in data.items():
+            for item in list(r):
+                f.write("%s,%s\n" % (i, item))
 
 
 if __name__ == '__main__':
--- recipe_web_scrape_manager.py
+++ recipe_web_scrape_manager.py
@@ -1,77 +1,82 @@
 from collections import defaultdict
-from Utilities.web_assist import amend_url, make_context, get_html_for_soup, find_in_url
+from Utilities.web_assist import prepend_root_to_url, make_context, get_html_for_soup, find_in_url, write_to_csv
 from datetime import datetime as dt
 import csv
+
 
 class RecipeWebScrapeManager:
 
     def __init__(self,
-                url: str = 'https://www.allrecipes.com/'):
+                 url: str = 'https://www.allrecipes.com',
+                 page_limit: int = 100):
         self.base_url = url
         self._context = make_context()
-        self._meal_categories = {}
+        self._meal_categories = None
+        self._recipe_link_dict = None
+        self._website_page_limit = page_limit
         print('Initialized at', dt.now())
 
     @property
     def meal_categories(self):
-        if self._meal_categories == {}:
-            self._get_meal_categories()
+        if self._meal_categories is None:
+            self._meal_categories = self._get_meal_categories()
         return self._meal_categories
 
-    def _get_meal_categories(self):
-        
-        soup = get_html_for_soup(self.base_url, self._context)
+    @property
+    def recipe_link_dict(self):
+        if self._recipe_link_dict is None:
+            self._recipe_link_dict = self.get_recipe_links()
+        return self._recipe_link_dict
 
+    def _get_meal_categories(self) -> dict:
+        recipe_categories = {}
+        soup = get_html_for_soup(self.base_url, self._context)
         tags = soup('a')
+
         for tag in tags:
             try:
                 if tag.get('class')[0] in 'see-all-heading':
                     link = tag.get('href')
                     cat = find_in_url(link)
-                    self._meal_categories[cat] = link
+                    recipe_categories[cat] = link
             except:
                 continue
-    
+        return recipe_categories
+
+    def get_recipe_links(self):
+        recipe_links_by_cat = defaultdict(set)
+
+        for cgy, lnk in self.meal_categories.items():
+            page_num = 1
+            valid_page = True
+
+            while valid_page and page_num <= self._website_page_limit:
+                page = '?page=' + str(page_num)
+                try:
+                    option_page = get_html_for_soup(lnk, self._context, page)
+                    print(lnk + page)
+                except:
+                    valid_page = False
+                    continue
+
+                tgs = option_page('a')
+                for tg in tgs:
+                    try:
+                        if ((tg.get('class')[0] in 'card__titleLink manual-link-behavior'
+                             and tg.get('aria-hidden')[0] in 'true') or tg.get('class')[0] in 'tout__imageLink'):
+                            raw_link = tg.get('href')
+
+                            if '/recipe/' in raw_link:
+                                corrected_link = prepend_root_to_url(raw_link, self.base_url)
+                                recipe_links_by_cat[cgy].add(corrected_link)
+                    except:
+                        continue
+                page_num += 1
+        return recipe_links_by_cat
+
 
 if __name__ == '__main__':
-    scr = RecipeWebScrapeManager()
-    # [print(i, r) for i, r in scr.meal_categories.items()]
-    root_url = 'https://www.allrecipes.com'
-    recipelist = defaultdict(set)
-    for cat, lnk in scr.meal_categories.items():
+    scr = RecipeWebScrapeManager(page_limit=15)
+    names = ['Category', 'URL']
+    write_to_csv('recipeURLs.csv', scr.recipe_link_dict, names)
 
-        i = 1
-        page_limit = 25
-        valid_page = True
-
-        while valid_page and i <= page_limit:
-            page = '?page='+str(i)
-            try:
-                option_page = get_html_for_soup(lnk, make_context(), page)
-                print(lnk+page)
-            except:
-                valid_page = False
-                continue
-
-            tgs = option_page('a')
-            for tg in tgs:
-                try:
-                    if ((tg.get('class')[0] in 'card__titleLink manual-link-behavior'
-                    and tg.get('aria-hidden')[0] in 'true') \
-                    or tg.get('class')[0] in 'tout__imageLink'):
-
-                        link = tg.get('href')
-                        corrected_link = amend_url(link, root_url)
-                        recipelist[cat].add(corrected_link)
-
-                except:
-                    continue
-            i += 1
-
-    with open('recipeURLs.csv', 'w') as f:
-        f.write("Category,URL\n")
-        for i, r in recipelist.items():
-            for item in list(r):
-                f.write("%s,%s\n"%(i,item))
-
-    [print(i, len(r)) for i, r in recipelist.items()]
