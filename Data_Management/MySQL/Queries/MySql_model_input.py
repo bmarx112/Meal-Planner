00ac2349ed57a1ee6709b08578eb08417fd4e991
Brad Marx <bradmarx112@gmail.com>
2021-12-21 20:02:10 -0700

    new query

--- Data_Management/MySQL/Queries/MySql_model_input.py
+++ Data_Management/MySQL/Queries/MySql_model_input.py
@@ -20,6 +20,35 @@ def model_nutrition_query_with_doubled(nutrients: list) -> str:
 
     return pull_nutrients
 
+def model_nutrition_query_with_doubled_and_tripled(nutrients: list) -> str:
+    
+    valid_elements = '(\'' + '\', \''.join(nutrients) + '\')'
+
+    pull_nutrients = f'''
+    SELECT m.Meal_Category, m.Recipe_Id, n.Element, n.Quantity
+    FROM Meal m
+    JOIN nutrition n
+    ON m.Recipe_Id = n.Recipe_Id
+    WHERE m.Meal_Category IN ('breakfast and brunch', 'lunch', 'dinner')
+    AND n.Element IN {valid_elements}
+    UNION ALL
+    SELECT m.Meal_Category, CONCAT(m.Recipe_Id, '_2') as Recipe_Id, n.Element, n.Quantity * 2 as Quantity
+    FROM Meal m
+    JOIN nutrition n
+    ON m.Recipe_Id = n.Recipe_Id
+    WHERE m.Meal_Category IN ('breakfast and brunch', 'lunch', 'dinner')
+    AND n.Element IN {valid_elements}
+    UNION ALL
+    SELECT m.Meal_Category, CONCAT(m.Recipe_Id, '_3') as Recipe_Id, n.Element, n.Quantity * 3 as Quantity
+    FROM Meal m
+    JOIN nutrition n
+    ON m.Recipe_Id = n.Recipe_Id
+    WHERE m.Meal_Category IN ('breakfast and brunch', 'lunch', 'dinner')
+    AND n.Element IN {valid_elements}
+    '''
+
+    return pull_nutrients
+
 def model_nutrition_query_doubled(nutrients: list) -> str:
     
     valid_elements = '(\'' + '\', \''.join(nutrients) + '\')'
